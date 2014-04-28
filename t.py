987d3f70b8713374c82d96a7c4c0f63e64d15ed0
Ernesto Paiser <ernesto.paiser@gmail.com>
2014-04-28 07:27:06 -0700

    add check routines to avoid duplicates
    add some routines to avoid duplicates.
    TODO: make a list of instruments, locations, components, and devices

--- t.py
+++ t.py
@@ -6,7 +6,7 @@ class Instrument(object):
     __storm_table__ = "instrument"
     id = Int(allow_none=False)
     name = Unicode(allow_none=False, primary=True)
-   
+    locations = Unicode()
     def __init__(self, name):
         self.name = name
         
@@ -15,35 +15,22 @@ class Loc(object):
     #instrument_id = Int(primary=True)
     instrument_name = Unicode(allow_none=False, primary=True)
     id = Int(allow_none=False)
-    #instrument = Reference(instrument_id, Instrument.id)
     instrument = Reference(instrument_name, Instrument.name)
     name = Unicode(allow_none=False)
+    #components = Unicode()
+    locations = Reference(Instrument.locations, name)
     
 class Location(Loc):
     def __init__(self, name):
         self.name = name
-        self.initialize()
-    
-    def initialize(self):
-        self._inmemory_locations = {}
-                
-    def __storm_loaded__(self):
-        print 'storm_loaded'
-        self.initialize()
-        #for location in self.locations:
-        self._inmemory_locations[self.name] = self.name
+        #self.initialize()
 
 class Component(Location):
     __storm_table__ = "component"
-    id = Int(allow_none=False)#primary=True)
-    #location_id = Int()    
+    id = Int(allow_none=False)#primary=True)   
     location_name = Unicode(allow_none=False) 
-    #location = Reference(location_id, Location.instrument_id)
-    ###location = Reference(location_name, Location.instrument_name)
     location = Reference(location_name, Location.name)
-    #component_instrument_id = Int()
-    #instrument = Reference(component_instrument_id, Instrument.id)
-
+    devices = Unicode()
     
     def __init__(self, name):
         self.name = name
@@ -54,9 +41,6 @@ class MotorManager(object):
         dbpars   = (scheme, user, passwd, hostname, port, dbname)
         database = create_database("%s://%s:%s@%s:%s/%s" % dbpars)        
         self.store = Store(database)
-        self.instruments = list()
-        self.location = None
-        self.component = None
         
     def create_alltables(self):
         try:
@@ -72,83 +56,80 @@ class MotorManager(object):
             pass
         
     def commit(self):
-        print 'commit'
         self.store.commit()
 
     def roolback(self):
         self.store.rollback()
         
-    def addinstrument(self, instrument): 
-        # NEED TO CHECK IF INSTRUMENT ALREADY EXISTS:
-        ##result = manager.store.find(Instrument)
-        result = self.store.find(Instrument, Instrument.name.is_in([unicode(instrument)]))
-        if result.count():
-            print 'Instrument %s Exists' % instrument
-            print 'SHOULD RETURN THE REQUESTED EXISTING INSTRUMENT'
-            return None# Instrument.instrument
-        #result.order_by(Instrument.name)
-        ##print 'result', result.count()
-        ##print 'result.names', list(result.values(Instrument.name))
-        ##for i in result:
-        ##    print 'i',i.name
-
-
-
-        newinstrument = Instrument(unicode(instrument))
-#         try:
-#             print self.instrument.name.count(newinstrument.name)#find(unicode(instrument))
-#         except:
-#             pass               
-        #newinstrument = Instrument(unicode(instrument))
+    def addinstrument(self, instrument_name=None): 
+        # CHECK IF INSTRUMENT ALREADY EXISTS:
+        instrument = self.getinstrument(instrument_name)
+        if instrument:
+            if instrument.name == instrument_name:
+                print 'Instrument %s already exists' % instrument.name
+                return instrument        
+        newinstrument = Instrument(unicode(instrument_name))
         print 'Creating New Instrument', newinstrument.name
-        #print self.store.find(Instrument, newinstrument.name).order_by(newinstrument.name).one()
-        
         self.commit()
-        #self.instrument = newinstrument
         return newinstrument
 
-    def addlocation(self, location, instrument):
-        #try:
-        newlocation = Location(unicode(location))
-        newlocation.instrument = instrument
-        #except:
-        #    pass
-        print 'Creating New Location', newlocation.name, 'in instrument', instrument.name
-        #self.inmemory_locations[instrument.name] = newlocation.name
-        #self.locations.append(newlocation.name)
-        self.commit()
-        return newlocation
+    def addlocation(self, location_name=None, instrument=None):
+        if instrument:
+            location = self.getlocation(location_name)
+            if location:
+                if location.name == location_name:
+                    print 'Location %s already exists in' % location.name, location.instrument.name
+                    return location
+            newlocation = Location(unicode(location_name))
+            newlocation.instrument = instrument
+            print 'Creating New Location', newlocation.name, 'in instrument', instrument.name
+            self.commit()
+            return newlocation
+        return None
 
-    def addcomponent(self, component, location=None):
-        newcomponent = self.store.add(Component(unicode(component)))
-        newcomponent.location = location
-        print 'lll', location.instrument.name
-        #print self.store.find(Instrument, Instrument.name.is_in([unicode(location.instrument.name)]))#.order_by('instrument_name').one()
-        #print self.store.find(Component)#, newcomponent.name == unicode(newcomponent.instrument.name))
-        
-        print 'kiki'
-        #print newcomponent.instrument.find(newcomponent.instrument.name)
-            #print 'ERROR: Instrument Exist!'
-
-        #print location.find(unicode(location))
-#             print 'ERROR: Location Exist!'
-#             return None
-        #print newcomponent.name.find(newcomponent.name)
-#             print 'ERROR: Component Exist!'
-            #return None        
-        
-        
-        newcomponent.instrument = location.instrument
-        print 'Creating New Component', newcomponent.name, 'in location', location.name, 'in instrument', newcomponent.instrument.name
-        #print self.store.find(Instrument, newcomponent.instrument.name).order_by(newcomponent.instrument.name).one()
-        #print self.store.find(Component, 'instrument_name').order_by('instrument_name').one()
-#         print [type(component.name) for component in self.store.find(Instrument).order_by(Instrument.name)]
-#         print 'here'
-#         instr = self.get_instruments(self.store)#, info_classes=[Instrument]).one()
-#         print instr.name
-        self.commit()     
-        return newcomponent
+    def addcomponent(self, component_name=None, location=None):
+        if location:
+            component = self.getcomponent(component_name)
+            if component:
+                if component.name == component_name:
+                    print 'Component %s already exists in' % component.name, component.location.name
+                    return component
+            newcomponent = self.store.add(Component(unicode(component_name)))
+            newcomponent.location = location
+            newcomponent.instrument = location.instrument
+            print 'Creating New Component', newcomponent.name, 'in location', location.name, 'in instrument', newcomponent.instrument.name
+            self.commit()     
+            return newcomponent
+        return None
     
+    def getinstrument(self, instrument_name):
+        return self.store.find(Instrument, Instrument.name == unicode(instrument_name)).one()
+
+    def getlocation(self, location_name):
+        return self.store.find(Location, Location.name == unicode(location_name)).one()
+   
+    def getcomponent(self, component_name):
+        return self.store.find(Component, Component.name == unicode(component_name)).one()
+    
+    def getinstruments(self):
+        result = self.store.find(Instrument, Instrument.name==unicode('*'))
+        result.order_by(Instrument.name)
+        return result.count()
+    
+    def getlocations(self, instrument):
+        print self.store.find(Instrument, Instrument.name)
+        for i in self.store.find(Instrument, Instrument.name):#.values():
+              print 'i', i#.one()
+        #print 'n', n
+#         locations = list()
+#         for loc in Location.name:
+#             locations.append(loc)
+#         return loc
+    
+    def getcomponents(self, location):
+        pass
+         
+        #print res.one().count()
 #     def get_instruments(store, info_classes=None):
 #         print 'here'
 #         where = []
@@ -170,17 +151,16 @@ if __name__ == '__main__':
     manager = MotorManager(scheme, user, passwd, hostname, port, dbname)
     manager.create_alltables()
 
-
     amo = manager.addinstrument('AMO')
     cxi = manager.addinstrument('CXI')
-    #amo  = manager.addinstrument('AMO')
-    #print amo.name.find(unicode('AMO'))
-#             print 'ERROR: Location Exist!'
-#             return None
-        
-    #amo  = manager.addinstrument('AMO')
     lamp = manager.addlocation('LAMP', amo)
-    #print lamp.name.find(unicode('LAMP'))
     sl02 = manager.addcomponent('slits02', lamp)
+    #sl02 = manager.addcomponent('slits02', lamp)
     sb2 = manager.addlocation('SB2', cxi)
     sl01 = manager.addcomponent('slits01', sb2)
+    print '\nNEED TO FIND INSTRUMENTS, LOCATIONS AND COMPONENTS...\n'
+    #print manager.getinstruments()
+    
+    Instrument.locations = ReferenceSet(Instrument.name, Location.name)
+    print amo.locations.count()
+    print manager.getlocations(amo)
